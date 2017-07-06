7750ad8a73d86b8a51c3f916e98312322f4d29d2
erik_rohkohl <erikrohkohl@googlemail.com>
2017-07-06 16:54:50 +0200

    Issue #3, replace all cnf vec! with HashSet, DONE

--- src/modelling/n_queens.rs
+++ src/modelling/n_queens.rs
@@ -1,7 +1,5 @@
 use std::collections::HashSet;
 
-//use backtracking::sat_solver as solver;
-
 pub fn at_most_one(xs:Vec<i32>) -> HashSet<Vec<i32>>{
     if xs.len() == 1{
         let mut min_clause:HashSet<Vec<i32>> = HashSet::new();
@@ -25,6 +23,7 @@ pub fn at_most_one(xs:Vec<i32>) -> HashSet<Vec<i32>>{
     }
 }
 
+
 pub fn at_least_one(xs:Vec<i32>) -> Vec<i32>{
     let n = xs.len() + 1;
     let mut cnf:Vec<i32> = Vec::with_capacity(n);
@@ -34,36 +33,37 @@ pub fn at_least_one(xs:Vec<i32>) -> Vec<i32>{
     }
     return cnf;
 }
-/*
 
-pub fn at_least_one_queen_in_every_row(n:usize) -> Vec<Vec<i32>>{
-    let mut cnf:Vec<Vec<i32>> = Vec::with_capacity(n);
+
+pub fn at_least_one_queen_in_every_row(n:usize) -> HashSet<Vec<i32>>{
+    let mut cnf:HashSet<Vec<i32>> = HashSet::new();
     for i in 1..n + 1{
         let mut xs:Vec<i32> = Vec::with_capacity(n);
         for j in 1..n + 1 {
             let var: i32 = (i as i32) * 10 + (j as i32);
             xs.push(var);
         }
-        cnf.push(at_least_one(xs))
+        cnf.insert(at_least_one(xs));
     }
     return cnf;
 }
 
-pub fn at_least_one_queen_in_every_column(n:usize) -> Vec<Vec<i32>>{
-    let mut cnf:Vec<Vec<i32>> = Vec::with_capacity(n);
+pub fn at_least_one_queen_in_every_column(n:usize) -> HashSet<Vec<i32>>{
+    let mut cnf:HashSet<Vec<i32>> = HashSet::new();
     for i in 1..n + 1{
         let mut xs:Vec<i32> = Vec::with_capacity(n);
         for j in 1..n + 1 {
             let var: i32 = (j as i32) * 10 + (i as i32);
             xs.push(var);
         }
-        cnf.push(at_least_one(xs))
+        cnf.insert(at_least_one(xs));
     }
     return cnf;
 }
 
-pub fn at_most_one_queen_in_every_row(n:usize) -> Vec<Vec<i32>>{
-    let mut cnf:Vec<Vec<i32>> = Vec::with_capacity(n);
+
+pub fn at_most_one_queen_in_every_row(n:usize) -> HashSet<Vec<i32>>{
+    let mut cnf:HashSet<Vec<i32>> = HashSet::new();
     for i in 1..n + 1 {
         for j in 1..n + 1 {
             for k in 1..n + 1 {
@@ -73,7 +73,9 @@ pub fn at_most_one_queen_in_every_row(n:usize) -> Vec<Vec<i32>>{
                     let var_2: i32 = (i as i32) * 10 + (k as i32);
                     xs.push(var_1);
                     xs.push(var_2);
-                    cnf.append(at_most_one(xs).as_mut());
+                    for i in at_most_one(xs){
+                        cnf.insert(i);
+                    }
                 }
             }
         }
@@ -81,8 +83,9 @@ pub fn at_most_one_queen_in_every_row(n:usize) -> Vec<Vec<i32>>{
     return cnf;
 }
 
-pub fn at_most_one_queen_in_every_column(n:usize) -> Vec<Vec<i32>>{
-    let mut cnf:Vec<Vec<i32>> = Vec::with_capacity(n);
+
+pub fn at_most_one_queen_in_every_column(n:usize) -> HashSet<Vec<i32>>{
+    let mut cnf:HashSet<Vec<i32>> = HashSet::new();
     for j in 1..n + 1 {
         for i in 1..n + 1 {
             for k in 1..n + 1 {
@@ -92,7 +95,9 @@ pub fn at_most_one_queen_in_every_column(n:usize) -> Vec<Vec<i32>>{
                     let var_2: i32 = (k as i32) * 10 + (j as i32);
                     xs.push(var_1);
                     xs.push(var_2);
-                    cnf.append(at_most_one(xs).as_mut());
+                    for i in at_most_one(xs){
+                        cnf.insert(i);
+                    }
                 }
             }
         }
@@ -100,20 +105,23 @@ pub fn at_most_one_queen_in_every_column(n:usize) -> Vec<Vec<i32>>{
     return cnf;
 }
 
-pub fn at_most_one_queen_in_every_diagonal(n:usize) -> Vec<Vec<i32>>{
-    let mut cnf:Vec<Vec<i32>> = Vec::with_capacity(n);
+
+pub fn at_most_one_queen_in_every_diagonal(n:usize) -> HashSet<Vec<i32>>{
+    let mut cnf:HashSet<Vec<i32>> = HashSet::new();
     for i in 1..n + 1 {
         for j in 1..n + 1 {
             for k in 1..n + 1 {
                 for m in 1..n + 1 {
-                    if i != k && j != m && i >= k && j >= m {
-                        if i + k == j + m || i - k == j - m {
+                    if i != k && j != m  {
+                        if i + k == j + m || i as i32 - k as i32 == j as i32 - m as i32 {
                             let mut xs: Vec<i32> = Vec::with_capacity(n);
                             let var_1: i32 = (i as i32) * 10 + (j as i32);
                             let var_2: i32 = (k as i32) * 10 + (m as i32);
                             xs.push(var_1);
                             xs.push(var_2);
-                            cnf.append(at_most_one(xs).as_mut());
+                            for clause in at_most_one(xs){
+                                cnf.insert(clause);
+                            }
                         }
                     }
                 }
@@ -123,12 +131,13 @@ pub fn at_most_one_queen_in_every_diagonal(n:usize) -> Vec<Vec<i32>>{
     return cnf;
 }
 
-pub fn generate_cnf(n:usize) -> Vec<Vec<i32>>{
-    let mut cnf:Vec<Vec<i32>> = Vec::with_capacity(n);
-    cnf.append(at_least_one_queen_in_every_column(n).as_mut());
-    cnf.append(at_least_one_queen_in_every_row(n).as_mut());
-    cnf.append(at_most_one_queen_in_every_row(n).as_mut());
-    cnf.append(at_most_one_queen_in_every_column(n).as_mut());
-    cnf.append(at_most_one_queen_in_every_diagonal(n).as_mut());
+
+pub fn generate_cnf(n:usize) -> HashSet<Vec<i32>>{
+    let mut cnf:HashSet<Vec<i32>> = HashSet::new();
+    for i in at_least_one_queen_in_every_column(n){cnf.insert(i);};
+    for i in at_least_one_queen_in_every_row(n){cnf.insert(i);};
+    for i in at_most_one_queen_in_every_row(n){cnf.insert(i);};
+    for i in at_most_one_queen_in_every_column(n){cnf.insert(i);};
+    for i in at_most_one_queen_in_every_diagonal(n){cnf.insert(i);};
     return cnf;
-}*/
+}
--- tests/n_queens_test.rs
+++ tests/n_queens_test.rs
@@ -1,164 +1,205 @@
 extern crate dpll;
 
+use std::collections::HashSet;
+
 use dpll::modelling::n_queens as queens;
 use dpll::backtracking::sat as solver;
 
-/*
 #[test]
 fn at_least_one_queen_in_every_row_case_one(){
     let n = 2;
-    let expected:Vec<Vec<i32>> = vec![vec![11, 12], vec![21, 22]];
-    let result:Vec<Vec<i32>> = queens::at_least_one_queen_in_every_row(n);
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![11, 12]);
+    expected.insert(vec![21, 22]);
+
+    let result:HashSet<Vec<i32>> = queens::at_least_one_queen_in_every_row(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_least_one_queen_in_every_row_case_two(){
     let n = 4;
-    let expected:Vec<Vec<i32>> =
-        vec![vec![11, 12, 13, 14], vec![21, 22, 23, 24], vec![31, 32, 33, 34], vec![41, 42, 43, 44]];
-    let result:Vec<Vec<i32>> = queens::at_least_one_queen_in_every_row(n);
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![11, 12, 13, 14]);
+    expected.insert(vec![21, 22, 23, 24]);
+    expected.insert(vec![31, 32, 33, 34]);
+    expected.insert(vec![41, 42, 43, 44]);
+
+    let result:HashSet<Vec<i32>> = queens::at_least_one_queen_in_every_row(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_least_one_queen_in_every_row_case_three(){
     let n = 8;
-    let expected:Vec<Vec<i32>> =
-            vec![
-                vec![11, 12, 13, 14, 15, 16, 17, 18]
-                ,vec![21, 22, 23, 24, 25, 26, 27, 28]
-                ,vec![31, 32, 33, 34, 35, 36, 37, 38]
-                ,vec![41, 42, 43, 44, 45, 46, 47, 48]
-                ,vec![51, 52, 53, 54, 55, 56, 57, 58]
-                ,vec![61, 62, 63, 64, 65, 66, 67, 68]
-                ,vec![71, 72, 73, 74, 75, 76, 77, 78]
-                ,vec![81, 82, 83, 84, 85, 86, 87, 88]
-            ];
-    let result:Vec<Vec<i32>> = queens::at_least_one_queen_in_every_row(n);
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![11, 12, 13, 14, 15, 16, 17, 18]);
+    expected.insert(vec![21, 22, 23, 24, 25, 26, 27, 28]);
+    expected.insert(vec![31, 32, 33, 34, 35, 36, 37, 38]);
+    expected.insert(vec![41, 42, 43, 44, 45, 46, 47, 48]);
+    expected.insert(vec![51, 52, 53, 54, 55, 56, 57, 58]);
+    expected.insert(vec![61, 62, 63, 64, 65, 66, 67, 68]);
+    expected.insert(vec![71, 72, 73, 74, 75, 76, 77, 78]);
+    expected.insert(vec![81, 82, 83, 84, 85, 86, 87, 88]);
+
+    let result:HashSet<Vec<i32>> = queens::at_least_one_queen_in_every_row(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_least_one_queen_in_every_column_case_one(){
     let n = 2;
-    let expected:Vec<Vec<i32>> =
-        vec![vec![11, 21], vec![12, 22]];
-    let result:Vec<Vec<i32>> = queens::at_least_one_queen_in_every_column(n);
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![11, 21]);
+    expected.insert(vec![12, 22]);
+
+    let result:HashSet<Vec<i32>> = queens::at_least_one_queen_in_every_column(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_least_one_queen_in_every_column_case_two(){
     let n = 4;
-    let expected:Vec<Vec<i32>> =
-        vec![vec![11, 21, 31, 41], vec![12, 22, 32, 42], vec![13, 23, 33, 43], vec![14, 24, 34, 44]];
-    let result:Vec<Vec<i32>> = queens::at_least_one_queen_in_every_column(n);
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![11, 21, 31, 41]);
+    expected.insert(vec![12, 22, 32, 42]);
+    expected.insert(vec![13, 23, 33, 43]);
+    expected.insert(vec![14, 24, 34, 44]);
+
+    let result:HashSet<Vec<i32>> = queens::at_least_one_queen_in_every_column(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_least_one_queen_in_every_column_case_three(){
     let n = 8;
-    let expected:Vec<Vec<i32>> =
-        vec![
-             vec![11, 21, 31, 41, 51, 61, 71, 81]
-            ,vec![12, 22, 32, 42, 52, 62, 72, 82]
-            ,vec![13, 23, 33, 43, 53, 63, 73, 83]
-            ,vec![14, 24, 34, 44, 54, 64, 74, 84]
-            ,vec![15, 25, 35, 45, 55, 65, 75, 85]
-            ,vec![16, 26, 36, 46, 56, 66, 76, 86]
-            ,vec![17, 27, 37, 47, 57, 67, 77, 87]
-            ,vec![18, 28, 38, 48, 58, 68, 78, 88]
-        ];
-    let result:Vec<Vec<i32>> = queens::at_least_one_queen_in_every_column(n);
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![11, 21, 31, 41, 51, 61, 71, 81]);
+    expected.insert(vec![12, 22, 32, 42, 52, 62, 72, 82]);
+    expected.insert(vec![13, 23, 33, 43, 53, 63, 73, 83]);
+    expected.insert(vec![14, 24, 34, 44, 54, 64, 74, 84]);
+    expected.insert(vec![15, 25, 35, 45, 55, 65, 75, 85]);
+    expected.insert(vec![16, 26, 36, 46, 56, 66, 76, 86]);
+    expected.insert(vec![17, 27, 37, 47, 57, 67, 77, 87]);
+    expected.insert(vec![18, 28, 38, 48, 58, 68, 78, 88]);
+
+    let result:HashSet<Vec<i32>> = queens::at_least_one_queen_in_every_column(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_most_one_queen_in_every_row_case_one(){
     let n = 2;
-    let expected:Vec<Vec<i32>> = vec![vec![-11, -12], vec![-21, -22]];
-    let result:Vec<Vec<i32>> = queens::at_most_one_queen_in_every_row(n);
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![-11, -12]);
+    expected.insert(vec![-21, -22]);
+    let result:HashSet<Vec<i32>> = queens::at_most_one_queen_in_every_row(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_most_one_queen_in_every_row_case_two(){
     let n = 4;
-    let expected:Vec<Vec<i32>> =
-        vec![
-            vec![-11, -12], vec![-11, -13], vec![-11, -14]
-            , vec![-12, -13], vec![-12, -14]
-            , vec![-13, -14]
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![-11, -12]);
+    expected.insert(vec![-11, -13]);
+    expected.insert(vec![-11, -14]);
+    expected.insert(vec![-12, -13]);
+    expected.insert(vec![-12, -14]);
+    expected.insert(vec![-13, -14]);
 
-            , vec![-21, -22], vec![-21, -23], vec![-21, -24]
-            , vec![-22, -23], vec![-22, -24]
-            , vec![-23, -24]
+    expected.insert(vec![-21, -22]);
+    expected.insert(vec![-21, -23]);
+    expected.insert(vec![-21, -24]);
+    expected.insert(vec![-22, -23]);
+    expected.insert(vec![-22, -24]);
+    expected.insert(vec![-23, -24]);
 
-            , vec![-31, -32], vec![-31, -33], vec![-31, -34]
-            , vec![-32, -33], vec![-32, -34]
-            , vec![-33, -34]
+    expected.insert(vec![-31, -32]);
+    expected.insert(vec![-31, -33]);
+    expected.insert(vec![-31, -34]);
+    expected.insert(vec![-32, -33]);
+    expected.insert(vec![-32, -34]);
+    expected.insert(vec![-33, -34]);
 
-            , vec![-41, -42], vec![-41, -43], vec![-41, -44]
-            , vec![-42, -43], vec![-42, -44]
-            , vec![-43, -44]
+    expected.insert(vec![-41, -42]);
+    expected.insert(vec![-41, -43]);
+    expected.insert(vec![-41, -44]);
+    expected.insert(vec![-42, -43]);
+    expected.insert(vec![-42, -44]);
+    expected.insert(vec![-43, -44]);
 
-        ];
-    let result:Vec<Vec<i32>> = queens::at_most_one_queen_in_every_row(n);
+    let result:HashSet<Vec<i32>> = queens::at_most_one_queen_in_every_row(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_most_one_queen_in_every_column_case_one(){
     let n = 2;
-    let expected:Vec<Vec<i32>> = vec![vec![-11, -21], vec![-12, -22]];
-    let result:Vec<Vec<i32>> = queens::at_most_one_queen_in_every_column(n);
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![-11, -21]);
+    expected.insert(vec![-12, -22]);
+
+    let result:HashSet<Vec<i32>> = queens::at_most_one_queen_in_every_column(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_most_one_queen_in_every_column_case_two(){
     let n = 4;
-    let expected:Vec<Vec<i32>> =
-        vec![
-            vec![-11, -21], vec![-11, -31], vec![-11, -41]
-            , vec![-21, -31], vec![-21, -41]
-            , vec![-31, -41]
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![-11, -21]);
+    expected.insert(vec![-11, -31]);
+    expected.insert(vec![-11, -41]);
+    expected.insert(vec![-21, -31]);
+    expected.insert(vec![-21, -41]);
+    expected.insert(vec![-31, -41]);
 
-            , vec![-12, -22], vec![-12, -32], vec![-12, -42]
-            , vec![-22, -32], vec![-22, -42]
-            , vec![-32, -42]
+    expected.insert(vec![-12, -22]);
+    expected.insert(vec![-12, -32]);
+    expected.insert(vec![-12, -42]);
+    expected.insert(vec![-22, -32]);
+    expected.insert(vec![-22, -42]);
+    expected.insert(vec![-32, -42]);
 
-            , vec![-13, -23], vec![-13, -33], vec![-13, -43]
-            , vec![-23, -33], vec![-23, -43]
-            , vec![-33, -43]
+    expected.insert(vec![-13, -23]);
+    expected.insert(vec![-13, -33]);
+    expected.insert(vec![-13, -43]);
+    expected.insert(vec![-23, -33]);
+    expected.insert(vec![-23, -43]);
+    expected.insert(vec![-33, -43]);
 
-            , vec![-14, -24], vec![-14, -34], vec![-14, -44]
-            , vec![-24, -34], vec![-24, -44]
-            , vec![-34, -44]
+    expected.insert(vec![-14, -24]);
+    expected.insert(vec![-14, -34]);
+    expected.insert(vec![-14, -44]);
+    expected.insert(vec![-24, -34]);
+    expected.insert(vec![-24, -44]);
+    expected.insert(vec![-34, -44]);
 
-        ];
-    let result:Vec<Vec<i32>> = queens::at_most_one_queen_in_every_column(n);
+    let result:HashSet<Vec<i32>> = queens::at_most_one_queen_in_every_column(n);
     assert_eq!(result, expected);
 }
 
 #[test]
 fn at_most_one_queen_in_every_diagonal_case_one(){
     let n = 2;
-    let expected:Vec<Vec<i32>> = vec![vec![-11, -21], vec![-12, -22]];
-    let result:Vec<Vec<i32>> = queens::at_most_one_queen_in_every_diagonal(n);
+    let mut expected:HashSet<Vec<i32>> = HashSet::new();
+    expected.insert(vec![-12, -21]);
+    expected.insert(vec![-11, -22]);
+
+    let result:HashSet<Vec<i32>> = queens::at_most_one_queen_in_every_diagonal(n);
     assert_eq!(result, expected);
 }
+
 
 #[test]
 fn generate_cnf_case_one(){
     let n:usize = 4;
-    let cnf:Vec<Vec<i32>> = queens::generate_cnf(n);
-    println!("{:?}", cnf);
+    let cnf:HashSet<Vec<i32>> = queens::generate_cnf(n);
+    //println!("{:?}", cnf);
 
     let part_assign: Vec<i32> = Vec::with_capacity(n * n);
     let (is_sat, model):(bool, Vec<i32>) = solver::dpll(cnf, part_assign);
+    println!("{:?}", model);
     assert!(is_sat);
 }
-*/
